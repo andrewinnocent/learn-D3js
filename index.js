c91426e4c4a29faddca32fcf25c3bda7c0461130
andrewinnocent <innocent.andrew@gmail.com>
2018-05-10 12:01:57 -0400

    Simple Bar Chart
    Created a bar chart using SVG.

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const dataset = [1, 2, 3, 4, 5];
+const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-d3.select('body') // parent
-    .selectAll('p') // child
-    .data(dataset) // loads the data for further processing; the data is bound to the `p` selection.
-    .enter() // "each element in the data array is paired with the corresponding node in the selection. If there are fewer nodes than data, the extra data elements form the enter selection, which you can instantiate by appending to the enter selection."
-    .append('p') // appends paragraph for each data element
-    // .text('D3 is awesome!!');
-    .text(function(d) { return `I\'m data number: ${d}`; }); // specified the text for each `p` appended
+const svgWidth = 500, svgHeight = 300, barPadding = 5; // chaining variables! What?! So cool! This sets the SVG dimensions in the DOM
+const barWidth = (svgWidth / dataset.length); // 55.55556
+
+
+const svg = d3.select('svg')
+    .attr("width", svgWidth) // 500
+    .attr("height", svgHeight); // 300
+
+const barChart = svg.selectAll("rect") // `rect` is an svg default for `rectangle`
+    .data(dataset) // loads data
+    .enter() // prepared each datum for further action
+    .append("rect") // each datum is bound to a `rect`
+    .attr("y", function(d) {
+         return svgHeight - d
+    }) // each `rect`'s attr.
+    .attr("height", function(d) {
+        return d;
+    })
+    .attr("width", barWidth - barPadding) // 50.5556
+    .attr("transform", function (d, i) { //
+        const translate = [barWidth * i, 0]; // barWidth * index, height below x axis
+        return "translate("+ translate +")"; // this allows each bar to begin relative to the previous bar instead of being stacked upon each other.
+    });
