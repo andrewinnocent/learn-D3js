2e88d609de56cb8805d4d8c482be79897d64da52
andrewinnocent <innocent.andrew@gmail.com>
2018-05-13 12:13:18 -0400

    Create a Pie Chart

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-var svgWidth = 600, svgHeight = 500;
+var data = [
+  {"platform": "Android", "percentage": 40.11},
+  {"platform": "Windows", "percentage": 36.69},
+  {"platform": "iOS", "percentage": 13.06}
+];
 
-var svg = d3.select("svg")
-   .attr("width", svgWidth)
-   .attr("height", svgHeight)
-   .attr("class", "svg-container")
+var svgWidth = 500, svgHeight = 300, radius =  Math.min(svgWidth, svgHeight) / 2; // take the min and divide by 2 for the center point of the chart in the SVG container
 
-var line = svg.append("line")
-   .attr("x1", 100) // starting position from left of the svg container
-   .attr("y1", 50) // starting position from top of the svg container
-   .attr("x2", 500) // ending position from left of the svg container
-   .attr("y2", 75) // ending position from top of the svg container
-   .attr("stroke", "red") // stroke attribute is required for the line to be visible
-   .attr("stroke-width", 5); // in pixels
+var svg = d3.select('svg')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
-var rect = svg.append("rect")
-   .attr("x", 200) // starting position from left of the svg container
-   .attr("y", 100) // starting position from top of the svg container
-   .attr("width", 200)
-   .attr("height", 100)
-   .attr("fill", "#9B95FF");
+//Create group element to hold pie chart
+var g = svg.append("g")
+    .attr("transform", "translate(" + radius + "," + radius + ")"); // translates the chart to the center of the SVG element
 
-var circle = svg.append("circle")
-   .attr("cx", 200) // position is from the center of the circle
-   .attr("cy", 300) // position is from the center of the circle
-   .attr("r", 80) // `r` is `radius` - starting from the center to the border of the circle
-   .attr("fill", "#7CE8D5");
+var color = d3.scaleOrdinal(d3.schemeCategory10); // built-in method for color options (10 of them)
+
+var pie = d3.pie().value(function(d) {
+    console.log(d); // object
+     return d.percentage;
+   });
+
+var path = d3.arc()
+    .outerRadius(radius) // defines the boundaries of the arc
+    .innerRadius(0); // defines the boundaries of the arc
+
+var arc = g.selectAll("arc")
+    .data(pie(data))
+    .enter()
+    .append("g");
+
+arc.append("path")
+    .attr("d", path)
+    .attr("fill", function(d) {
+      console.log(d); // array
+      return color(d.data.percentage);
+    });
+
+var label = d3.arc()
+    .outerRadius(radius)
+    .innerRadius(0);
+
+arc.append("text")
+    .attr("transform", function(d) {
+        return "translate(" + label.centroid(d) + ")";
+    })
+    .attr("text-anchor", "middle")
+    .text(function(d) {
+      return d.data.platform + ":" + d.data.percentage + "%";
+    });
